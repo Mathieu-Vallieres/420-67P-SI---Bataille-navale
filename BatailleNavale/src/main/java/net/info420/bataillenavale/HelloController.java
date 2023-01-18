a464167e38e0dd98617ddd9a6670e8d28e78b68e
ThuGapy <61449423+ThuGapy@users.noreply.github.com>
2023-01-18 10:56:37 -0500

    Mise à jour du projet

---
+++ BatailleNavale/src/main/java/net/info420/bataillenavale/HelloController.java
@@ -0,0 +1,14 @@
+package net.info420.bataillenavale;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
