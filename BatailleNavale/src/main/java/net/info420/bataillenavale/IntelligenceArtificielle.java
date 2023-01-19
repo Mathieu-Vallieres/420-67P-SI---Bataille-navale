21bccaa8a9061b0e71be9642eff99d8d74010f09
ChristoV5565 <77989805+ChristoV5565@users.noreply.github.com>
2023-01-19 12:55:22 -0500

    Update IntelligenceArtificielle.java

--- BatailleNavale/src/main/java/net/info420/bataillenavale/IntelligenceArtificielle.java
+++ BatailleNavale/src/main/java/net/info420/bataillenavale/IntelligenceArtificielle.java
@@ -15,13 +15,39 @@ public class IntelligenceArtificielle {
         this.tableauTirs = BatailleNavale.grilleJoueur;
     }
 
+    public void setDifficulty_Hard()
+    {
+        this.difficulty_Hard = true;
+    }
+
+    public void setDifficulty_Easy()
+    {
+        this.difficulty_Hard = false;
+    }
+
     //Difficulté du jeu. L'ordinateur joue mieux si cette variable est true
     public boolean difficulty_Hard;
+
     //Grille qui contient tous les endroits où l'ordinateur a déjà tiré
     public int[][] tableauTirs;
+
     //Permet de générer des nombres aléatoires
     Random rand = new Random();
 
+    //Sert à stocker le dernier tir pour le tir intelligent
+    public Vector2 dernierTir;
+
+    //Entre en mode tir intelligent
+    public boolean tirIntelligent;
+
+    //Stocke l'ID du bateau qui est visé par le tir intelligent
+    public int IDBateauTirIntelligent;
+
+    /**
+     * Permet de générer des coordonnées aléatoires selon le mode de difficulté
+     *
+     * @return un vecteur contenant les coordonnées générées
+     */
     public Vector2 genererCoordsAleatoire()
     {
         Vector2 coords;
@@ -82,6 +108,53 @@ public class IntelligenceArtificielle {
         return  coords;
     }
 
+    //Renvoie les coordonnées où l'ordi a tiré pour pouvoir faire l'update graphique dans la grille
+    public Vector2 tirer()
+    {
+        if(tirIntelligent)
+        {
+
+        }
+        else
+        {
+            do {
+                Vector2 coords = genererCoordsAleatoire();
+            }
+            //S'assure que le programme ne tire pas 2 fois au même endroit
+            while (tableauTirs[coords.GetX()][coords.GetY()] != 6 && tableauTirs[coords.GetX()][coords.GetY] != 7);
+
+            //À partir d'ici on peut tirer
+            if(tableauTirs[coords.GetX()][coords.GetY()])
+            {
+                //Bateau touché :
+                //1. Stocke l'ID du bateau pour pouvoir le cibler avec le tir intelligent
+                IDBateauTirIntelligent = tableauTirs[coords.GetX()][coords.GetY()];
+                //2. Marque la case comme une section de bateau touchée
+                tableauTirs[coords.GetX()][coords.GetY()] = 6;
+                //3. Stocke la position du dernier tir pour
+                dernierTir = new Vector2(coords.GetX(),coords.GetY());
+                //4. Tester si le bateau est coulé
+                if(couler(tableauTirs, IDBateauTirIntelligent))
+                {
+                    //5a. Si le bateau est coulé, désactiver le tir intelligent
+                    tirIntelligent = false;
+                }
+                else
+                {
+                    //5b. Si le bateau n'est pas coulé, activer le tir intelligent
+                    tirIntelligent = true;
+                }
+            }
+            else
+            {
+                //Atterri dans l'eau
+                tableauTirs[coords.GetX()][coords.GetY] = 7;
+            }
+        }
+
+        return coords;
+    }
+
     /**
      * Retourne un nombre aleatoire entre a inclus et b exclus
      */
@@ -91,26 +164,11 @@ public class IntelligenceArtificielle {
     }
 
     /**
-     * Fonction qui retourne un tableau qui contient les coordonn�es du tir al�atoire de l'ordinateur
+     * Vérifie si un bateau est coulé (n'est plus présent dans la grille)
      *
-     * @return un tableau contenant la ligne et la colonne du tir de l'ordinateur
-     */
-    public static int[] tirOrdinateur()
-    {
-        int ligne = randRange(0,10);
-        int colonne = randRange(0,10);
-
-        int[] tableau = {ligne, colonne};
-
-        return tableau;
-    }
-
-    /**
-     * V�rifie si un bateau est coul� (n'est plus pr�sent dans la grille)
-     *
-     * @param grille Grille dans laquelle la v�rification est effectu�e
-     * @param bateau Bateau qui est v�rifi�
-     * @return Un bool�en qui sera vrai si un bateau n'apparait plus dans la grille
+     * @param grille Grille dans laquelle la vérification est effectuée
+     * @param bateau Bateau qui est vérifié
+     * @return Un booléen qui sera vrai si un bateau n'apparait plus dans la grille
      */
     public static boolean couler(int [][] grille, int bateau)
     {
@@ -120,13 +178,13 @@ public class IntelligenceArtificielle {
             {
                 if(grille[i][j] == bateau)
                 {
-                    //La fonction indique que le bateau n'est pas coul� lorsqu'elle trouve au moins une fois son num�ro dans la grille
+                    //La fonction indique que le bateau n'est pas coulé lorsqu'elle trouve au moins une fois son numéro dans la grille
                     return false;
                 }
             }
         }
 
-        //Le bateau a �t� coul�; son chiffre n'apparait pas dans la grille
+        //Le bateau a été coulé; son chiffre n'apparait pas dans la grille
         return true;
     }
 
